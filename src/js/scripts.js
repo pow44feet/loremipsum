ec07a4cd0854498bee8e409fd0e2de5efba6d769
Oleg Andrianov <andrianov.ov@yandex.ru>
2022-07-13 17:09:18 +0300

    made mobile menu appear animation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,7 @@
 const header = document.querySelector(".header");
+const burger = document.querySelector(".burger");
+const body = document.body;
+
 
 $(window).scroll(function() {
   if ($(this).scrollTop() > 1){
@@ -9,4 +12,14 @@ $(window).scroll(function() {
     }
 });
 
-
+burger.addEventListener("click", function () {
+  if (burger.classList.contains("burger--active")) {
+    burger.classList.remove("burger--active");
+    header.classList.remove("menu--active");
+    body.classList.remove("body--blocked");
+  } else {
+    burger.classList.add("burger--active");
+    header.classList.add("menu--active");
+    body.classList.add("body--blocked");
+  }
+});
